311cc2228f10856701a04b2f7ce25b117d7ba44d
DragonRoyal <79556380+DragonRoyal@users.noreply.github.com>
2021-08-15 00:35:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="SomeApi-DragonRoyal",
-    version="0.0.1",
-    author="DragonRoyal",
-    description="Light weight official wrapper of SomeApi in python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+from distutils.core import setup
+setup(
+  name = 'someapi_wrapper',         # How you named your package folder (MyLib)
+  packages = ['someapi_wrapper'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Light weight official wrapper of SomeApi in python',   # Give a short description about your library
+  author = 'DragonRoyal',                   # Type in your name
+  author_email = 'aarav.singhania50@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/DragonRoyal/SomeApi_pw',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/DragonRoyal/SomeApi_pw/archive/refs/tags/V_0.1.tar.gz',    # I explain this later on
+  keywords = ['SomeApi', 'Api', 'Image Api'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'asyncio',
+          'aiohttp',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
 )
