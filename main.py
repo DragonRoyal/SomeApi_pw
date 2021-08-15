8d0961e1d7199cc28e1ccff655dc4de7baf9446d
DragonRoyal <79556380+DragonRoyal@users.noreply.github.com>
2021-08-14 22:38:05 -0500

    Update and rename math.py to main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+import aiohttp
+import asyncio
+class Images:
+    async def dog(self):
+        return "https://someapi.xyz/dog/image"
+
+class Json:
+
+
+    async def dog():
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get("https://someapi.xyz/json/dogs") as r:
+            
+                data = await r.json()
+        print(data["fact"])
+        return data["fact"]
+    async def quote(amogus):
+
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get("https://someapi.xyz/json/quote") as r:
+            
+                data = await r.json()
+        if amogus=="author":
+            return data["author"]
+            
+        else:
+            return data["quote"]
+            
+    async def mocktext(text):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f"https://someapi.xyz/mocktext?text={text}") as r:
+            
+                data = await r.json()
+        return data["text"]
+class Canvas:
+    async def invert(avatar):
+        return f"https://someapi.xyz/invert?avatar={avatar}"
+
+    async def greyscale(avatar):
+        return f"https://someapi.xyz/greyscale?avatar={avatar}"
+
+    async def wanted(avatar):
+        return f"https://someapi.xyz/wanted?avatar={avatar}"
+
+    async def alert(text):
+        return f"https://someapi.xyz/alert?text={text}"
+
+    async def god_temp(text):
+        return f"https://someapi.xyz/god_temp?text={text}"
+
+    async def biden(text):
+        return f"https://someapi.xyz/biden?text={text}"
+        
+    async def gay(avatar):
+        return f"https://someapi.xyz/rainbow?avatar={avatar}"
+
+    async def billy(text):
+        return f"https://someapi.xyz/billy?text={text}"
+
+    async def pacman(text):
+        return f"https://someapi.xyz/pacman/text?text={text}"
+
+    async def hitler(avatar):
+        return f"https://someapi.xyz/hitler?avatar={avatar}"
+
+
