b7402c7eb8fa4f06bff3dc94448d12be5a670342
DragonRoyal <79556380+DragonRoyal@users.noreply.github.com>
2021-08-15 23:00:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   keywords = ['SomeApi', 'Api', 'Image Api'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'aiohttp',
-          'oudated',
+       
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
